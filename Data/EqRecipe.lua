073dbd074f199bd56c0663f49a221ae5a36093aa
Kier Davis <me@kierdavis.com>
2021-03-06 01:34:46 +0000

    Reflow

--- Data/EqRecipe.lua
+++ Data/EqRecipe.lua
@@ -37,43 +37,193 @@ function CanICraftThis.EqRecipe:registerAll()
     end
     self:register(args)
   end
-  register { id = 1,  name = "Axe",               traitCategoryId = ITEM_TRAIT_TYPE_CATEGORY_WEAPON }
-  register { id = 2,  name = "Mace",              traitCategoryId = ITEM_TRAIT_TYPE_CATEGORY_WEAPON }
-  register { id = 3,  name = "Sword",             traitCategoryId = ITEM_TRAIT_TYPE_CATEGORY_WEAPON }
-  register { id = 4,  name = "Battle Axe",        traitCategoryId = ITEM_TRAIT_TYPE_CATEGORY_WEAPON }
-  register { id = 5,  name = "Maul",              traitCategoryId = ITEM_TRAIT_TYPE_CATEGORY_WEAPON }
-  register { id = 6,  name = "Greatsword",        traitCategoryId = ITEM_TRAIT_TYPE_CATEGORY_WEAPON }
-  register { id = 7,  name = "Dagger",            traitCategoryId = ITEM_TRAIT_TYPE_CATEGORY_WEAPON }
-  register { id = 8,  name = "Cuirass",           traitCategoryId = ITEM_TRAIT_TYPE_CATEGORY_ARMOR }
-  register { id = 9,  name = "Sabatons",          traitCategoryId = ITEM_TRAIT_TYPE_CATEGORY_ARMOR }
-  register { id = 10, name = "Gauntlets",         traitCategoryId = ITEM_TRAIT_TYPE_CATEGORY_ARMOR }
-  register { id = 11, name = "Helm",              traitCategoryId = ITEM_TRAIT_TYPE_CATEGORY_ARMOR }
-  register { id = 12, name = "Greaves",           traitCategoryId = ITEM_TRAIT_TYPE_CATEGORY_ARMOR }
-  register { id = 13, name = "Pauldron",          traitCategoryId = ITEM_TRAIT_TYPE_CATEGORY_ARMOR }
-  register { id = 14, name = "Girdle",            traitCategoryId = ITEM_TRAIT_TYPE_CATEGORY_ARMOR }
-  register { id = 15, name = "Robe",              traitCategoryId = ITEM_TRAIT_TYPE_CATEGORY_ARMOR, researchLineName = "Robe & Jerkin" }
-  register { id = 16, name = "Jerkin",            traitCategoryId = ITEM_TRAIT_TYPE_CATEGORY_ARMOR, researchLineName = "Robe & Jerkin" }
-  register { id = 17, name = "Shoes",             traitCategoryId = ITEM_TRAIT_TYPE_CATEGORY_ARMOR }
-  register { id = 18, name = "Gloves",            traitCategoryId = ITEM_TRAIT_TYPE_CATEGORY_ARMOR }
-  register { id = 19, name = "Hat",               traitCategoryId = ITEM_TRAIT_TYPE_CATEGORY_ARMOR }
-  register { id = 20, name = "Breeches",          traitCategoryId = ITEM_TRAIT_TYPE_CATEGORY_ARMOR }
-  register { id = 21, name = "Epaulets",          traitCategoryId = ITEM_TRAIT_TYPE_CATEGORY_ARMOR }
-  register { id = 22, name = "Sash",              traitCategoryId = ITEM_TRAIT_TYPE_CATEGORY_ARMOR }
-  register { id = 23, name = "Jack",              traitCategoryId = ITEM_TRAIT_TYPE_CATEGORY_ARMOR }
-  register { id = 24, name = "Boots",             traitCategoryId = ITEM_TRAIT_TYPE_CATEGORY_ARMOR }
-  register { id = 25, name = "Bracers",           traitCategoryId = ITEM_TRAIT_TYPE_CATEGORY_ARMOR }
-  register { id = 26, name = "Helmet",            traitCategoryId = ITEM_TRAIT_TYPE_CATEGORY_ARMOR }
-  register { id = 27, name = "Guards",            traitCategoryId = ITEM_TRAIT_TYPE_CATEGORY_ARMOR }
-  register { id = 28, name = "Arm Cops",          traitCategoryId = ITEM_TRAIT_TYPE_CATEGORY_ARMOR }
-  register { id = 29, name = "Belt",              traitCategoryId = ITEM_TRAIT_TYPE_CATEGORY_ARMOR }
-  register { id = 30, name = "Bow",               traitCategoryId = ITEM_TRAIT_TYPE_CATEGORY_WEAPON }
-  register { id = 31, name = "Inferno Staff",     traitCategoryId = ITEM_TRAIT_TYPE_CATEGORY_WEAPON }
-  register { id = 32, name = "Ice Staff",         traitCategoryId = ITEM_TRAIT_TYPE_CATEGORY_WEAPON }
-  register { id = 33, name = "Lightning Staff",   traitCategoryId = ITEM_TRAIT_TYPE_CATEGORY_WEAPON }
-  register { id = 34, name = "Restoration Staff", traitCategoryId = ITEM_TRAIT_TYPE_CATEGORY_WEAPON }
-  register { id = 35, name = "Shield",            traitCategoryId = ITEM_TRAIT_TYPE_CATEGORY_ARMOR }
-  register { id = 36, name = "Necklace",          traitCategoryId = ITEM_TRAIT_TYPE_CATEGORY_JEWELRY }
-  register { id = 37, name = "Ring",              traitCategoryId = ITEM_TRAIT_TYPE_CATEGORY_JEWELRY }
+  register {
+    id = 1,
+    name = "Axe",
+    traitCategoryId = ITEM_TRAIT_TYPE_CATEGORY_WEAPON,
+  }
+  register {
+    id = 2,
+    name = "Mace",
+    traitCategoryId = ITEM_TRAIT_TYPE_CATEGORY_WEAPON,
+  }
+  register {
+    id = 3,
+    name = "Sword",
+    traitCategoryId = ITEM_TRAIT_TYPE_CATEGORY_WEAPON,
+  }
+  register {
+    id = 4,
+    name = "Battle Axe",
+    traitCategoryId = ITEM_TRAIT_TYPE_CATEGORY_WEAPON,
+  }
+  register {
+    id = 5,
+    name = "Maul",
+    traitCategoryId = ITEM_TRAIT_TYPE_CATEGORY_WEAPON,
+  }
+  register {
+    id = 6,
+    name = "Greatsword",
+    traitCategoryId = ITEM_TRAIT_TYPE_CATEGORY_WEAPON,
+  }
+  register {
+    id = 7,
+    name = "Dagger",
+    traitCategoryId = ITEM_TRAIT_TYPE_CATEGORY_WEAPON,
+  }
+  register {
+    id = 8,
+    name = "Cuirass",
+    traitCategoryId = ITEM_TRAIT_TYPE_CATEGORY_ARMOR,
+  }
+  register {
+    id = 9,
+    name = "Sabatons",
+    traitCategoryId = ITEM_TRAIT_TYPE_CATEGORY_ARMOR,
+  }
+  register {
+    id = 10,
+    name = "Gauntlets",
+    traitCategoryId = ITEM_TRAIT_TYPE_CATEGORY_ARMOR,
+  }
+  register {
+    id = 11,
+    name = "Helm",
+    traitCategoryId = ITEM_TRAIT_TYPE_CATEGORY_ARMOR,
+  }
+  register {
+    id = 12,
+    name = "Greaves",
+    traitCategoryId = ITEM_TRAIT_TYPE_CATEGORY_ARMOR,
+  }
+  register {
+    id = 13,
+    name = "Pauldron",
+    traitCategoryId = ITEM_TRAIT_TYPE_CATEGORY_ARMOR,
+  }
+  register {
+    id = 14,
+    name = "Girdle",
+    traitCategoryId = ITEM_TRAIT_TYPE_CATEGORY_ARMOR,
+  }
+  register {
+    id = 15,
+    name = "Robe",
+    traitCategoryId = ITEM_TRAIT_TYPE_CATEGORY_ARMOR,
+    researchLineName = "Robe & Jerkin",
+  }
+  register {
+    id = 16,
+    name = "Jerkin",
+    traitCategoryId = ITEM_TRAIT_TYPE_CATEGORY_ARMOR,
+    researchLineName = "Robe & Jerkin",
+  }
+  register {
+    id = 17,
+    name = "Shoes",
+    traitCategoryId = ITEM_TRAIT_TYPE_CATEGORY_ARMOR,
+  }
+  register {
+    id = 18,
+    name = "Gloves",
+    traitCategoryId = ITEM_TRAIT_TYPE_CATEGORY_ARMOR,
+  }
+  register {
+    id = 19,
+    name = "Hat",
+    traitCategoryId = ITEM_TRAIT_TYPE_CATEGORY_ARMOR,
+  }
+  register {
+    id = 20,
+    name = "Breeches",
+    traitCategoryId = ITEM_TRAIT_TYPE_CATEGORY_ARMOR,
+  }
+  register {
+    id = 21,
+    name = "Epaulets",
+    traitCategoryId = ITEM_TRAIT_TYPE_CATEGORY_ARMOR,
+  }
+  register {
+    id = 22,
+    name = "Sash",
+    traitCategoryId = ITEM_TRAIT_TYPE_CATEGORY_ARMOR,
+  }
+  register {
+    id = 23,
+    name = "Jack",
+    traitCategoryId = ITEM_TRAIT_TYPE_CATEGORY_ARMOR,
+  }
+  register {
+    id = 24,
+    name = "Boots",
+    traitCategoryId = ITEM_TRAIT_TYPE_CATEGORY_ARMOR,
+  }
+  register {
+    id = 25,
+    name = "Bracers",
+    traitCategoryId = ITEM_TRAIT_TYPE_CATEGORY_ARMOR,
+  }
+  register {
+    id = 26,
+    name = "Helmet",
+    traitCategoryId = ITEM_TRAIT_TYPE_CATEGORY_ARMOR,
+  }
+  register {
+    id = 27,
+    name = "Guards",
+    traitCategoryId = ITEM_TRAIT_TYPE_CATEGORY_ARMOR,
+  }
+  register {
+    id = 28,
+    name = "Arm Cops",
+    traitCategoryId = ITEM_TRAIT_TYPE_CATEGORY_ARMOR,
+  }
+  register {
+    id = 29,
+    name = "Belt",
+    traitCategoryId = ITEM_TRAIT_TYPE_CATEGORY_ARMOR,
+  }
+  register {
+    id = 30,
+    name = "Bow",
+    traitCategoryId = ITEM_TRAIT_TYPE_CATEGORY_WEAPON,
+  }
+  register {
+    id = 31,
+    name = "Inferno Staff",
+    traitCategoryId = ITEM_TRAIT_TYPE_CATEGORY_WEAPON,
+  }
+  register {
+    id = 32,
+    name = "Ice Staff",
+    traitCategoryId = ITEM_TRAIT_TYPE_CATEGORY_WEAPON,
+  }
+  register {
+    id = 33,
+    name = "Lightning Staff",
+    traitCategoryId = ITEM_TRAIT_TYPE_CATEGORY_WEAPON,
+  }
+  register {
+    id = 34,
+    name = "Restoration Staff",
+    traitCategoryId = ITEM_TRAIT_TYPE_CATEGORY_WEAPON,
+  }
+  register {
+    id = 35,
+    name = "Shield",
+    traitCategoryId = ITEM_TRAIT_TYPE_CATEGORY_ARMOR,
+  }
+  register {
+    id = 36,
+    name = "Necklace",
+    traitCategoryId = ITEM_TRAIT_TYPE_CATEGORY_JEWELRY,
+  }
+  register {
+    id = 37,
+    name = "Ring",
+    traitCategoryId = ITEM_TRAIT_TYPE_CATEGORY_JEWELRY,
+  }
 end
 
 CanICraftThis.EqRecipe:registerAll()
