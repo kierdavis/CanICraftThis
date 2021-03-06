7c8b222524b27f5c21693cd47fc7d27ef23edd86
Kier Davis <me@kierdavis.com>
2021-03-06 01:56:30 +0000

    Avoid mirroring recipe classification information in recipesBySubCategory

--- Cache/StyleCollectibleCache.lua
+++ Cache/StyleCollectibleCache.lua
@@ -1,5 +1,5 @@
 CanICraftThis.StyleCollectibleCache = {
-  version = 2,
+  version = 3,
 }
 
 function CanICraftThis.StyleCollectibleCache:open()
@@ -9,70 +9,6 @@ function CanICraftThis.StyleCollectibleCache:open()
   setmetatable(instance, { __index = self })
   return instance
 end
-
-local recipesBySubCategory = {
-  ["Staff"] = {
-    ["staff$"] = {
-      CanICraftThis.EqRecipe:fromName("Inferno Staff"),
-      CanICraftThis.EqRecipe:fromName("Ice Staff"),
-      CanICraftThis.EqRecipe:fromName("Lightning Staff"),
-      CanICraftThis.EqRecipe:fromName("Restoration Staff"),
-    },
-  },
-  ["Two-Handed"] = {
-    ["battle axe$"] = {CanICraftThis.EqRecipe:fromName("Battle Axe")},
-    ["maul$"] = {CanICraftThis.EqRecipe:fromName("Maul")},
-    ["greatsword$"] = {CanICraftThis.EqRecipe:fromName("Greatsword")},
-  },
-  ["Bow"] = {
-    ["bow$"] = {CanICraftThis.EqRecipe:fromName("Bow")},
-  },
-  ["Shield"] = {
-    ["shield$"] = {CanICraftThis.EqRecipe:fromName("Shield")},
-  },
-  ["One-Handed"] = {
-    ["axe$"] = {CanICraftThis.EqRecipe:fromName("Axe")},
-    ["mace$"] = {CanICraftThis.EqRecipe:fromName("Mace")},
-    ["sword$"] = {CanICraftThis.EqRecipe:fromName("Sword")},
-    ["dagger$"] = {CanICraftThis.EqRecipe:fromName("Dagger")},
-  },
-  ["Feet"] = {
-    ["sabatons$"] = {CanICraftThis.EqRecipe:fromName("Sabatons")},
-    ["shoes$"] = {CanICraftThis.EqRecipe:fromName("Shoes")},
-    ["boots$"] = {CanICraftThis.EqRecipe:fromName("Boots")},
-  },
-  ["Legs"] = {
-    ["greaves$"] = {CanICraftThis.EqRecipe:fromName("Greaves")},
-    ["breeches$"] = {CanICraftThis.EqRecipe:fromName("Breeches")},
-    ["guards$"] = {CanICraftThis.EqRecipe:fromName("Guards")},
-  },
-  ["Waist"] = {
-    ["girdle$"] = {CanICraftThis.EqRecipe:fromName("Girdle")},
-    ["sash$"] = {CanICraftThis.EqRecipe:fromName("Sash")},
-    ["belt$"] = {CanICraftThis.EqRecipe:fromName("Belt")},
-  },
-  ["Chest"] = {
-    ["cuirass$"] = {CanICraftThis.EqRecipe:fromName("Cuirass")},
-    ["robe$"] = {CanICraftThis.EqRecipe:fromName("Robe")},
-    ["jerkin$"] = {CanICraftThis.EqRecipe:fromName("Jerkin")},
-    ["jack$"] = {CanICraftThis.EqRecipe:fromName("Jack")},
-  },
-  ["Hands"] = {
-    ["gauntlets$"] = {CanICraftThis.EqRecipe:fromName("Gauntlets")},
-    ["gloves$"] = {CanICraftThis.EqRecipe:fromName("Gloves")},
-    ["bracers$"] = {CanICraftThis.EqRecipe:fromName("Bracers")},
-  },
-  ["Head"] = {
-    ["helm$"] = {CanICraftThis.EqRecipe:fromName("Helm")},
-    ["hat$"] = {CanICraftThis.EqRecipe:fromName("Hat")},
-    ["helmet$"] = {CanICraftThis.EqRecipe:fromName("Helmet")},
-  },
-  ["Shoulders"] = {
-    ["pauldrons?$"] = {CanICraftThis.EqRecipe:fromName("Pauldron")},
-    ["epaulets$"] = {CanICraftThis.EqRecipe:fromName("Epaulets")},
-    ["arm cops$"] = {CanICraftThis.EqRecipe:fromName("Arm Cops")},
-  },
-}
 
 local nameExceptions = {
   ["daedric skirt"] = "daedric breeches",
@@ -87,16 +23,28 @@ local function detectStyle(collectibleName)
   end
 end
 
-local function detectRecipes(collectibleName, subCategoryName)
-  local recipesForSubCategory = recipesBySubCategory[subCategoryName]
-  if #recipesForSubCategory == 1 then
-    local _, recipesForCollectible = pairs(recipesForSubCategory)()
-    return recipesForCollectible
+local function makeRecipeDetectorForSubCategory(subCategoryName)
+  local patternMap = {}
+  for _, recipe in pairs(CanICraftThis.EqRecipe.instancesByName) do
+    if recipe.styleCollectibleSubCategory == subCategoryName then
+      if patternMap[recipe.styleCollectibleNamePattern] == nil then
+        patternMap[recipe.styleCollectibleNamePattern] = {}
+      end
+      table.insert(patternMap[recipe.styleCollectibleNamePattern], recipe)
+    end
+  end
+  if #patternMap == 1 then
+    -- There is only one recipe name pattern that can possibly appear in this subcategory.
+    local _, recipeSet = pairs(patternMap)()
+    return function(_) return recipeSet end
   else
-    local pattern, recipesForCollectible
-    for pattern, recipesForCollectible in pairs(recipesForSubCategory) do
-      if string.find(collectibleName, pattern) then
-        return recipesForCollectible
+    -- Multiple recipes could appear in this subcategory, so we must pattern-match the collectible name.
+    return function(collectibleName)
+      local pattern, recipeSet
+      for pattern, recipeSet in pairs(patternMap) do
+        if string.find(collectibleName, pattern) then
+          return recipeSet
+        end
       end
     end
   end
@@ -115,6 +63,7 @@ function CanICraftThis.StyleCollectibleCache:populate()
       local subCategoryIndex
       for subCategoryIndex = 1, numSubCategories do
         local subCategoryName, numCollectibles = GetCollectibleSubCategoryInfo(categoryIndex, subCategoryIndex)
+        local detectRecipes = makeRecipeDetectorForSubCategory(subCategoryName)
         local collectibleIndex
         for collectibleIndex = 1, numCollectibles do
           local collectibleId = GetCollectibleId(categoryIndex, subCategoryIndex, collectibleIndex)
@@ -122,7 +71,7 @@ function CanICraftThis.StyleCollectibleCache:populate()
           local collectibleName = string.gsub(string.lower(collectibleName), "%s*%d+$", "")
           local collectibleName = nameExceptions[collectibleName] or collectibleName
           local style = detectStyle(collectibleName)
-          local recipes = detectRecipes(collectibleName, subCategoryName)
+          local recipes = detectRecipes(collectibleName)
           if style ~= nil and recipes ~= nil then
             local recipe
             for _, recipe in ipairs(recipes) do
--- Data/EqRecipe.lua
+++ Data/EqRecipe.lua
@@ -11,6 +11,13 @@ function CanICraftThis.EqRecipe:register(instance)
   CanICraftThis.assert(instance.numTraits ~= nil, "instance.numTraits ~= nil")
   instance.namePattern = CanICraftThis.literalPattern(instance.name)
   instance.writTextPattern = "craft an? [^;]+ " .. instance.namePattern
+  if instance.skill.equipmentInfo.usesStyle then
+    CanICraftThis.assert(instance.styleCollectibleSubCategory ~= nil, "instance.styleCollectibleSubCategory ~= nil")
+    instance.styleCollectibleNamePattern = (instance.styleCollectibleNamePattern or instance.namePattern) .. "$"
+  else
+    CanICraftThis.assert(instance.styleCollectibleSubCategory == nil, "instance.styleCollectibleSubCategory == nil")
+    CanICraftThis.assert(instance.styleCollectibleNamePattern == nil, "instance.styleCollectibleNamePattern == nil")
+  end
   self.instancesByName[instance.name] = instance
 end
 
@@ -41,178 +48,218 @@ function CanICraftThis.EqRecipe:registerAll()
     id = 1,
     name = "Axe",
     traitCategoryId = ITEM_TRAIT_TYPE_CATEGORY_WEAPON,
+    styleCollectibleSubCategory = "One-Handed",
   }
   register {
     id = 2,
     name = "Mace",
     traitCategoryId = ITEM_TRAIT_TYPE_CATEGORY_WEAPON,
+    styleCollectibleSubCategory = "One-Handed",
   }
   register {
     id = 3,
     name = "Sword",
     traitCategoryId = ITEM_TRAIT_TYPE_CATEGORY_WEAPON,
+    styleCollectibleSubCategory = "One-Handed",
   }
   register {
     id = 4,
     name = "Battle Axe",
     traitCategoryId = ITEM_TRAIT_TYPE_CATEGORY_WEAPON,
+    styleCollectibleSubCategory = "Two-Handed",
   }
   register {
     id = 5,
     name = "Maul",
     traitCategoryId = ITEM_TRAIT_TYPE_CATEGORY_WEAPON,
+    styleCollectibleSubCategory = "Two-Handed",
   }
   register {
     id = 6,
     name = "Greatsword",
     traitCategoryId = ITEM_TRAIT_TYPE_CATEGORY_WEAPON,
+    styleCollectibleSubCategory = "Two-Handed",
   }
   register {
     id = 7,
     name = "Dagger",
     traitCategoryId = ITEM_TRAIT_TYPE_CATEGORY_WEAPON,
+    styleCollectibleSubCategory = "One-Handed",
   }
   register {
     id = 8,
     name = "Cuirass",
     traitCategoryId = ITEM_TRAIT_TYPE_CATEGORY_ARMOR,
+    styleCollectibleSubCategory = "Chest",
   }
   register {
     id = 9,
     name = "Sabatons",
     traitCategoryId = ITEM_TRAIT_TYPE_CATEGORY_ARMOR,
+    styleCollectibleSubCategory = "Feet",
   }
   register {
     id = 10,
     name = "Gauntlets",
     traitCategoryId = ITEM_TRAIT_TYPE_CATEGORY_ARMOR,
+    styleCollectibleSubCategory = "Hands",
   }
   register {
     id = 11,
     name = "Helm",
     traitCategoryId = ITEM_TRAIT_TYPE_CATEGORY_ARMOR,
+    styleCollectibleSubCategory = "Head",
   }
   register {
     id = 12,
     name = "Greaves",
     traitCategoryId = ITEM_TRAIT_TYPE_CATEGORY_ARMOR,
+    styleCollectibleSubCategory = "Legs",
   }
   register {
     id = 13,
     name = "Pauldron",
     traitCategoryId = ITEM_TRAIT_TYPE_CATEGORY_ARMOR,
+    styleCollectibleSubCategory = "Shoulders",
+    styleCollectibleNamePattern = "pauldrons?",
   }
   register {
     id = 14,
     name = "Girdle",
     traitCategoryId = ITEM_TRAIT_TYPE_CATEGORY_ARMOR,
+    styleCollectibleSubCategory = "Waist",
   }
   register {
     id = 15,
     name = "Robe",
     traitCategoryId = ITEM_TRAIT_TYPE_CATEGORY_ARMOR,
     researchLineName = "Robe & Jerkin",
+    styleCollectibleSubCategory = "Chest",
   }
   register {
     id = 16,
     name = "Jerkin",
     traitCategoryId = ITEM_TRAIT_TYPE_CATEGORY_ARMOR,
     researchLineName = "Robe & Jerkin",
+    styleCollectibleSubCategory = "Chest",
   }
   register {
     id = 17,
     name = "Shoes",
     traitCategoryId = ITEM_TRAIT_TYPE_CATEGORY_ARMOR,
+    styleCollectibleSubCategory = "Feet",
   }
   register {
     id = 18,
     name = "Gloves",
     traitCategoryId = ITEM_TRAIT_TYPE_CATEGORY_ARMOR,
+    styleCollectibleSubCategory = "Hands",
   }
   register {
     id = 19,
     name = "Hat",
     traitCategoryId = ITEM_TRAIT_TYPE_CATEGORY_ARMOR,
+    styleCollectibleSubCategory = "Head",
   }
   register {
     id = 20,
     name = "Breeches",
     traitCategoryId = ITEM_TRAIT_TYPE_CATEGORY_ARMOR,
+    styleCollectibleSubCategory = "Legs",
   }
   register {
     id = 21,
     name = "Epaulets",
     traitCategoryId = ITEM_TRAIT_TYPE_CATEGORY_ARMOR,
+    styleCollectibleSubCategory = "Shoulders",
   }
   register {
     id = 22,
     name = "Sash",
     traitCategoryId = ITEM_TRAIT_TYPE_CATEGORY_ARMOR,
+    styleCollectibleSubCategory = "Waist",
   }
   register {
     id = 23,
     name = "Jack",
     traitCategoryId = ITEM_TRAIT_TYPE_CATEGORY_ARMOR,
+    styleCollectibleSubCategory = "Chest",
   }
   register {
     id = 24,
     name = "Boots",
     traitCategoryId = ITEM_TRAIT_TYPE_CATEGORY_ARMOR,
+    styleCollectibleSubCategory = "Feet",
   }
   register {
     id = 25,
     name = "Bracers",
     traitCategoryId = ITEM_TRAIT_TYPE_CATEGORY_ARMOR,
+    styleCollectibleSubCategory = "Hands",
   }
   register {
     id = 26,
     name = "Helmet",
     traitCategoryId = ITEM_TRAIT_TYPE_CATEGORY_ARMOR,
+    styleCollectibleSubCategory = "Head",
   }
   register {
     id = 27,
     name = "Guards",
     traitCategoryId = ITEM_TRAIT_TYPE_CATEGORY_ARMOR,
+    styleCollectibleSubCategory = "Legs",
   }
   register {
     id = 28,
     name = "Arm Cops",
     traitCategoryId = ITEM_TRAIT_TYPE_CATEGORY_ARMOR,
+    styleCollectibleSubCategory = "Shoulders",
   }
   register {
     id = 29,
     name = "Belt",
     traitCategoryId = ITEM_TRAIT_TYPE_CATEGORY_ARMOR,
+    styleCollectibleSubCategory = "Waist",
   }
   register {
     id = 30,
     name = "Bow",
     traitCategoryId = ITEM_TRAIT_TYPE_CATEGORY_WEAPON,
+    styleCollectibleSubCategory = "Bow",
   }
   register {
     id = 31,
     name = "Inferno Staff",
     traitCategoryId = ITEM_TRAIT_TYPE_CATEGORY_WEAPON,
+    styleCollectibleSubCategory = "Staff",
+    styleCollectibleNamePattern = "staff",
   }
   register {
     id = 32,
     name = "Ice Staff",
     traitCategoryId = ITEM_TRAIT_TYPE_CATEGORY_WEAPON,
+    styleCollectibleSubCategory = "Staff",
+    styleCollectibleNamePattern = "staff",
   }
   register {
     id = 33,
     name = "Lightning Staff",
     traitCategoryId = ITEM_TRAIT_TYPE_CATEGORY_WEAPON,
+    styleCollectibleSubCategory = "Staff",
+    styleCollectibleNamePattern = "staff",
   }
   register {
     id = 34,
     name = "Restoration Staff",
     traitCategoryId = ITEM_TRAIT_TYPE_CATEGORY_WEAPON,
+    styleCollectibleSubCategory = "Staff",
+    styleCollectibleNamePattern = "staff",
   }
   register {
     id = 35,
     name = "Shield",
     traitCategoryId = ITEM_TRAIT_TYPE_CATEGORY_ARMOR,
+    styleCollectibleSubCategory = "Shield",
   }
   register {
     id = 36,
