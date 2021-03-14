2f5455fbe6251f084037c89d172d9dd66d41ffa6
Kier Davis <me@kierdavis.com>
2021-03-14 18:41:28 +0000

    Fix crash when interacting with a set crafting station
    This happened because when interacting with a set crafting station,
    GetSmithingPatternInfo returns information about the set-specific
    recipes (e.g. "Adept Rider's Ring") in addition to the generic ones
    (e.g. "Ring"). These set-specific recipe names were unrecognised by
    EqRecipe:fromName, causing an assertion failure.
    
    Resolves #1.

--- Cache/CraftingStationCache.lua
+++ Cache/CraftingStationCache.lua
@@ -26,30 +26,35 @@ function CanICraftThis.CraftingStationCache:updateFromCurrentCraftingStation()
   if not skill.equipmentInfo then return end
   local recipeIndex
   for recipeIndex = 1, GetNumSmithingPatterns() do
-    local recipeName, _, _, numMaterials = GetSmithingPatternInfo(recipeIndex)
-    recipeName = recipeNameExceptions[recipeName] or recipeName
-    local recipe = CanICraftThis.EqRecipe:fromName(recipeName)
-    local requiredQuantityForMainMaterial = {}
-    local materialIndex
-    for materialIndex = 1, numMaterials do
-      local material, _, requiredQuantity, _, _, _, _, _, _, requiredPassiveAbility = GetSmithingPatternMaterialItemInfo(recipeIndex, materialIndex)
-      local material = CanICraftThis.Material:sanitise(material)
-      if CanICraftThis.Material:isEqMain(material) then
-        local prevRequiredPassiveAbility = self.data.requiredPassiveAbility[material]
-        if prevRequiredPassiveAbility ~= nil and prevRequiredPassiveAbility ~= requiredPassiveAbility then
-          CanICraftThis.reportUnexpected(
-            "multiple values of requiredPassiveAbility for same material " .. material .. ": " ..
-            tostring(prevRequiredPassiveAbility) .. ", " .. tostring(requiredPassiveAbility)
-          )
-        end
-        self.data.requiredPassiveAbility[material] = requiredPassiveAbility
-        local prevRequiredQuantity = requiredQuantityForMainMaterial[material]
-        if prevRequiredQuantity == nil or requiredQuantity < prevRequiredQuantity then
-          requiredQuantityForMainMaterial[material] = requiredQuantity
+    local recipeName, _, _, numMaterials, numTraitsRequired = GetSmithingPatternInfo(recipeIndex)
+    if numTraitsRequired == 0 then
+      recipeName = recipeNameExceptions[recipeName] or recipeName
+      local recipe = CanICraftThis.EqRecipe:fromName(recipeName)
+      local requiredQuantityForMainMaterial = {}
+      local materialIndex
+      for materialIndex = 1, numMaterials do
+        local material, _, requiredQuantity, _, _, _, _, _, _, requiredPassiveAbility = GetSmithingPatternMaterialItemInfo(recipeIndex, materialIndex)
+        local material = CanICraftThis.Material:sanitise(material)
+        if CanICraftThis.Material:isEqMain(material) then
+          local prevRequiredPassiveAbility = self.data.requiredPassiveAbility[material]
+          if prevRequiredPassiveAbility ~= nil and prevRequiredPassiveAbility ~= requiredPassiveAbility then
+            CanICraftThis.reportUnexpected(
+              "multiple values of requiredPassiveAbility for same material " .. material .. ": " ..
+              tostring(prevRequiredPassiveAbility) .. ", " .. tostring(requiredPassiveAbility)
+            )
+          end
+          self.data.requiredPassiveAbility[material] = requiredPassiveAbility
+          local prevRequiredQuantity = requiredQuantityForMainMaterial[material]
+          if prevRequiredQuantity == nil or requiredQuantity < prevRequiredQuantity then
+            requiredQuantityForMainMaterial[material] = requiredQuantity
+          end
         end
       end
+      self.data.requiredQuantity[recipe.id] = requiredQuantityForMainMaterial
+    else
+      -- This is a set-specific recipe (e.g. "Adept Rider's Ring").
+      -- Generic recipes (e.g. "Ring") will also be listed, so we can simply skip this.
     end
-    self.data.requiredQuantity[recipe.id] = requiredQuantityForMainMaterial
   end
 end
 
